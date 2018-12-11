9d2af8ca8e738d8b448655a51bb9710a4fc06a9d
Alvaro Valarezo <lfvalare@espol.edu.ec>
2018-12-11 13:33:58 -0500

    clases

--- ManejoComputadoras/src/emsablajeComputadoras/builder/AsusZenBookBuilder.java
+++ ManejoComputadoras/src/emsablajeComputadoras/builder/AsusZenBookBuilder.java
@@ -1,5 +1,36 @@
 package emsablajeComputadoras.builder;
 
-public class AsusZenBookBuilder {
+import emsamablajecomputadoras.creacional.Computador;
+import emsamablajecomputadoras.creacional.Mainboard;
+import emsamablajecomputadoras.creacional.SistemaOperativo;
+
+public class AsusZenBookBuilder extends CompuBuilder {
+	public 
+	@Override
+	public void definirCompu() {
+		// TODO Auto-generated method stub
+				SistemaOperativo os = new SistemaOperativo("Windows 10", 64, "Home");
+				Mainboard mainb = new Mainboard("Prime", "z370");
+				Computador c = new Computador(16, os, 500, mainb, "Asus", "ZenBook");
+	}
+
+	@Override
+	public void construirSO() {
+		// TODO Auto-generated method stub
+		SistemaOperativo os = new SistemaOperativo("Windows 10", 64, "Home");
+		compu.setOs(os);
+	}
+
+	@Override
+	public void construirMain() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Computador getComputador() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- ManejoComputadoras/src/emsablajeComputadoras/builder/AsusrogeBuilder.java
+++ ManejoComputadoras/src/emsablajeComputadoras/builder/AsusrogeBuilder.java
@@ -1,5 +1,37 @@
 package emsablajeComputadoras.builder;
 
-public class AsusrogeBuilder {
+import emsamablajecomputadoras.creacional.Computador;
+import emsamablajecomputadoras.creacional.Mainboard;
+import emsamablajecomputadoras.creacional.SistemaOperativo;
+
+public class AsusrogeBuilder extends CompuBuilder {
+	 protected Computador computador;
+	@Override
+	public void definirCompu() {
+		// TODO Auto-generated method stub
+		SistemaOperativo os = new SistemaOperativo("Windows 10", 64, "Pro");
+		Mainboard mainb = new Mainboard("Strix", "x99");
+		Computador c = new Computador(34, os, 1000, mainb, "Asus", "ROG");
+		
+	}
+
+	@Override
+	public void construirSO() {
+		// TODO Auto-generated method stub
+		SistemaOperativo os = new SistemaOperativo("Windows 10", 64, "Pro");
+		computador.setOs(os);
+	}
+
+	@Override
+	public void construirMain() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Computador getComputador() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
